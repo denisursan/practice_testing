1337a1f01374eaa76a9349b70d6752cf2ab1d922
Teodora Ispas <tispas@EN1411724.local>
2022-05-05 12:29:29 +0300

    Update with PUT

--- src/MyApp/JSONPlaceholder.js
+++ src/MyApp/JSONPlaceholder.js
@@ -1,52 +1,73 @@
-const requests = require('../basicRequests')
+const requests = require("../basicRequests");
 
-
-const URL = 'https://jsonplaceholder.typicode.com/'
-const USERS_URL = URL + 'users'
-const POSTS_URL = URL + 'posts'
+const URL = "https://jsonplaceholder.typicode.com/";
+const USERS_URL = URL + "users";
+const POSTS_URL = URL + "posts";
 const headers = {
-    'Content-Type': 'application/json',
-}
+  "Content-Type": "application/json",
+};
 var id;
 
-
 class JSONPlaceholder {
+  getUsers = async () => {
+    return await requests.get(USERS_URL, headers);
+  };
 
-    getUsers = async () => {
-        return await requests.get(USERS_URL, headers)
-    }
+  getPosts = async () => {
+    return await requests.get(POSTS_URL, headers);
+  };
 
-    getPosts = async () => {
-        return await requests.get(POSTS_URL, headers)
-    }
+  getSpecificPost = async (id) => {
+    return await requests.get(POSTS_URL + `/${id}`, headers);
+  };
 
-    createUser = async (body,headers)=>{
-       return await requests.post(POSTS_URL,body,headers)
-    }
-    updateUser = async(id,body,header) => {
-  
-        return await requests.patch(POSTS_URL+`/${id}`,body,header)
-    }
+  createUser = async (body, headers) => {
+    return await requests.post(POSTS_URL, body, headers);
+  };
+  updateUser = async (id, body, header) => {
+    return await requests.patch(POSTS_URL + `/${id}`, body, header);
+  };
 
-    deletePost = async(id) => {
-        return await requests.delete(POSTS_URL+`/${id}`)
-    }
+  deletePost = async (id) => {
+    return await requests.delete(POSTS_URL + `/${id}`);
+  };
 
-    async generateNewPostContent(id, newContent) {
+  async generateNewPostContent() {
+    for (var a = "", b = 36; a.length < 40; )
+      a += ((Math.random() * b) | 0).toString(b);
+    // var crypto = require("crypto");
+    // var id = crypto.randomBytes(20).toString('hex');
+    let body = JSON.stringify({
+      title: a,
+    });
+    return body;
+  }
 
-        return body
-    }
+  async findPostById() {
+    const response = await this.getSpecificPost(1);
+    console.log(response.data);
+    return id;
+  }
 
-    async findPost(body, title) {
+  async findPostByTitle(title) {
+    const posts = await this.getPosts();
+    const data = posts.data;
+    let id;
+    await data.forEach((el) => {
+      if (el.title === title) {
+        id = el.id;
+      }
+    });
+    return id;
+  }
 
-        return id
-    }
-
-    
-
-    updatePost = async (body, title, newContent) => {
-        let id = await this.findPost(body, title)
-        return await requests.put(POSTS_URL, this.generateNewPostContent(id, newContent), headers)
-    }
+  updatePost = async (title) => {
+    let id = await this.findPostByTitle(title);
+    return await requests.put(
+      POSTS_URL + `/${id}`,
+      await this.generateNewPostContent(),
+      headers
+    );
+  };
 }
-module.exports = new JSONPlaceholder()
+module.exports = new JSONPlaceholder();
--- test/JSONPlaceholder/posts.spec.js
+++ test/JSONPlaceholder/posts.spec.js
@@ -1,52 +1,79 @@
-const { expect } = require('chai');
-const JSONPlaceholder = require('../../src/MyApp/JSONPlaceholder')
-const body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-const title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
-const bodyPost = JSON.stringify({
-    title: 'Teodora',
-    body: 'bar',
-    userId: 1,
-  })
-  const bodyUpdate = JSON.stringify({
-    title: 'updatedTitle',
-  })
+// const { expect } = require("chai");
+const { expect } = require("chai").use(require("chai-json-schema"));
+const cypress = require("cypress");
+const requests = require("../../src/basicRequests");
 
-const headerPost= {
-    'Content-type': 'application/json; charset=UTF-8',
-  }
+const JSONPlaceholder = require("../../src/MyApp/JSONPlaceholder");
+const body =
+  "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto";
+const title =
+  "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
+const bodyPost = JSON.stringify({
+  title: "Teodora",
+  body: "bar",
+  userId: 1,
+});
+const bodyUpdate = JSON.stringify({
+  title: "updatedTitle",
+});
+
+const headerPost = {
+  "Content-type": "application/json; charset=UTF-8",
+};
 
 describe("Posts API Testing", async () => {
-    it("Get Posts", async () => {
-        let response = await JSONPlaceholder.getPosts()
-        // console.log(response.data[0]);
-        expect(response.status).equals(200);
-        console.log(response.data)
-       // expect(response.data).to.not.be.empty()
-        expect(response.data[0].title).to.contain('')
-    });
+  it("Get Posts", async () => {
+    let response = await JSONPlaceholder.getPosts();
+    // console.log(response.data[0]);
+    expect(response.status).equals(200);
+    console.log(response.data);
+    // expect(response.data).to.not.be.empty()
+    expect(response.data[0].title).to.contain("");
+  });
 
-    it("Create Posts", async () => {
-        let response = await JSONPlaceholder.createUser(bodyPost,headerPost)
-        console.log(response.data)
-        expect(response.data.title).to.equal('Teodora')
-        expect(response.data.body).to.equal('bar')
-        expect(response.data.userId).to.equal(1)
-        expect(response.status).to.equal(201)
-    });
+  it("Create Posts", async () => {
+    let response = await JSONPlaceholder.createUser(bodyPost, headerPost);
+    console.log(response.data);
+    expect(response.data.title).to.equal("Teodora");
+    expect(response.data.body).to.equal("bar");
+    expect(response.data.userId).to.equal(1);
+    expect(response.status).to.equal(201);
+  });
 
-    it("Update Posts", async () => {
-        let response = await JSONPlaceholder.updateUser(1,bodyUpdate,headerPost)
-        console.log(response.data)
-    });
+  it("Update Posts", async () => {
+    let response = await JSONPlaceholder.updateUser(1, bodyUpdate, headerPost);
+    console.log(response.data);
+  });
 
-    it("Delete Posts", async () => {
-        let response = await JSONPlaceholder.deletePost(1)
-        expect(response.status).to.equal(200)
-    });
+  it("Delete Posts", async () => {
+    let response = await JSONPlaceholder.deletePost(1);
+    expect(response.status).to.equal(200);
+  });
 
-    it.only("Update Posts with PUT", async () => {
-        let response = await JSONPlaceholder.updatePost(body,title,bodyUpdate)
-        console.log(response)
-    });
+  it("Update Posts with PUT", async () => {
+    let response = await JSONPlaceholder.updatePost(body, title, bodyUpdate);
+    console.log(response);
+  });
 
-})
+  it("Update post with PUT and random title", async () => {
+    let response = await JSONPlaceholder.updatePost(
+      "labore in ex et explicabo corporis aut quas"
+    );
+
+    let responseSchema = {
+      type: "object",
+      properties: {
+        title: {
+          type: "string",
+        },
+        id: {
+          type: ["string", "integer"],
+        },
+      },
+      required: ["title", "id"],
+    };
+    console.log(response.data);
+    expect(response.status).to.equal(200);
+    expect(response.data).to.be.jsonSchema(responseSchema);
+  });
+});
